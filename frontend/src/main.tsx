6aab6fe478e92dc8f48697aeba6b12990cd603c0
Simon Laubersheimer <simon.laubersheimer@gmail.com>
2025-03-04 21:12:42 +0100

    added api proxy to vite.config.ts

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,8 +3,25 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import axios from 'axios'
+
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+// Just a test :)
+function echo(msg) {
+	axios.post('/api/echo', { msg })
+		.then(response => {
+			console.log(response.data)
+		})
+		.catch(error => {
+			console.error(error)
+		})
+}
+
+echo("TEST")
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,20 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
+  build: {
+    assetsDir: 'static',
+  },
+  server: {
+    port: 3000,
+    cors: true,
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:5000/",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
