2ed9ba2e28389c8eed0d64c549f9de69474c524f
Simon Laubersheimer <simon.laubersheimer@gmail.com>
2025-03-04 21:17:52 +0100

    vite.config.ts comment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// https://dev.to/din0saur5/integrating-vite-with-flask-for-production-28af
 export default defineConfig({
   base: './',
   plugins: [react()],
