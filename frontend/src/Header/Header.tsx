c327af2819b6d5ff26784b3820ade31a07c7424b
jonahkraft <jonah.kraft@icloud.com>
2025-03-05 10:05:08 +0100

    Ordnerstruktur für css

--- frontend/src/Header/Header.tsx
+++ frontend/src/Header/Header.tsx
@@ -2,23 +2,21 @@ import './Header.css'
 
 function Header() {
     return (
-        <>
-            <nav>
-                <input type="checkbox" id="check"/>
-                <label htmlFor="check" className="checkbtn">
-                    <i className="fas fa-bars"></i>
-                </label>
-                <a href="#home" className="logo">Logo</a>
-                <ul id="navigation">
-                    <li><a href="#home" className="btn active">Home</a>
-                    </li>
-                    <li><a href="#about" className="btn">Blank</a></li>
-                    <li><a href="#menu" className="btn">Blank</a></li>
-                    <li><a href="#order" className="btn">Blank</a></li>
-                    <li><a href="#contact" className="btn">Blank</a></li>
-                </ul>
-            </nav>
-        </>
+        <nav>
+            <input type="checkbox" id="check"/>
+            <label htmlFor="check" className="checkbtn">
+                <i className="fas fa-bars"></i>
+            </label>
+            <a href="#home" className="logo">Logo</a>
+            <ul id="navigation">
+                <li><a href="#home" className="btn active">Home</a>
+                </li>
+                <li><a href="#about" className="btn">Blank</a></li>
+                <li><a href="#menu" className="btn">Blank</a></li>
+                <li><a href="#order" className="btn">Blank</a></li>
+                <li><a href="#contact" className="btn">Blank</a></li>
+            </ul>
+        </nav>
     )
 }
 
