15329e0b57387db9433b6ff2d917259e20b31b99
Maria Kalinichenko <144844634+kamari00@users.noreply.github.com>
2025-03-05 15:50:50 +0100

    Update Header.tsx

--- frontend/src/Header/Header.tsx
+++ frontend/src/Header/Header.tsx
@@ -2,19 +2,22 @@ import './Header.module.css'
 
 function Header() {
     return (
-        <nav>
-            <label htmlFor="check" className="checkbtn">
-                <i className="fas fa-bars"></i>
-            </label>
-            <a href="../../index.html" className="logo"><img src="../../public/favicon.svg" alt="Logo" width="20px" height="20px"/></a>
-            <ul id="navigation">
-                <li><a href="../../index.html" className="btn active">Home</a>
-                </li>
-                <li><a href="../../visualization.html" className="btn">Datenvisualisierung</a></li>
-                <li><a href="../../login.html" className="btn">Login</a></li>
-            </ul>
-        </nav>
-    )
+        <header>
+            <nav>
+                <label htmlFor="check" className="checkbtn">
+                    <i className="fas fa-bars"></i>
+                </label>
+                <a href="../../index.html" className="logo">
+                    <img src="../../public/favicon.svg" alt="Logo" width="20px" height="20px"/>
+                </a>
+                <ul id="navigation">
+                    <li><a href="" className="btn active">Home</a></li>
+                    <li><a href="#" className="btn">Datenvisualisierung</a></li>
+                    <li><a href="../../login.html" className="btn">Login</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
 }
 
-export default Header
+export default Header;
