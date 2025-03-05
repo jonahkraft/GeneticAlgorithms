3b79254fdaa39b7bddd7dbbeb3aa1b3e9b9c2d41
jonahkraft <jonah.kraft@icloud.com>
2025-03-05 16:29:56 +0100

    header gefixed

--- frontend/src/Header/Header.tsx
+++ frontend/src/Header/Header.tsx
@@ -11,8 +11,8 @@ function Header() {
                     <img src="../../public/favicon.svg" alt="Logo" width="20px" height="20px"/>
                 </a>
                 <ul id="navigation">
-                    <li><a href="" className="btn active">Home</a></li>
-                    <li><a href="#" className="btn">Datenvisualisierung</a></li>
+                    <li><a href="../../index.html" className="btn active">Home</a></li>
+                    <li><a href="../../visualization.html" className="btn">Datenvisualisierung</a></li>
                     <li><a href="../../login.html" className="btn">Login</a></li>
                 </ul>
             </nav>
