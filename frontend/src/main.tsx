d2ad67145c2d233ad5c2a75b82ec6e9b7f6c42fe
Julius Dörhöfer <dj.doerhoefer@t-online.de>
2025-03-05 16:07:35 +0100

    unnötiger import removed

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,8 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import HomePage from './HomePage/HomePage.tsx'
 
-import axios from 'axios'
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HomePage />
