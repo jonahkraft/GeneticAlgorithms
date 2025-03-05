aed70589b4012ac2dd1b405c41eee94766e67079
Simon Laubersheimer <simon.laubersheimer@gmail.com>
2025-03-05 14:55:23 +0100

    removed echo test function

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -11,16 +11,5 @@ createRoot(document.getElementById('root')!).render(
   </StrictMode>,
 )
 
-// Just a test :)
-function echo(msg: any) {
-	axios.post('/api/echo', { msg })
-		.then(response => {
-			console.log(response.data)
-		})
-		.catch(error => {
-			console.error(error)
-		})
-}
 
-echo("TEST")
 
