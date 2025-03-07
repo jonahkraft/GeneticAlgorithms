7b82c388fb2aff1b16468cc06f0aec539fa9425c
pradau <pradau@students.uni-mainz.de>
2025-03-07 11:20:29 +0100

    Added Restrictions with Roles

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify, send_from_directory, request, jsonify
 from flask_cors import CORS
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import get_jwt_identity
 import json
 import os
 
@@ -8,17 +10,34 @@ CORS(app)  # Allow frontend to communicate with backend
 
 import api
 
+protected_paths_analyst = ["register.html"]  # Pfade auf den der Analyst keinen Zugriff haben soll
+protected_paths_simulation = ["register.html"]  # Pfad auf den der Simulationsexperte keinen Zugriff haben soll
 @app.route("/", defaults={"path": ""})
 @app.route("/<path:path>")
 def serve_react(path: str):
-    """Serve the react frontend (only called by flask)"""
-    if path:
-        absolute_path = os.path.join(app.static_folder, path)
+    if path in protected_paths_analyst or path in protected_paths_simulation:
+        return serve_protected_react(path)
+    else:
+        return serve_unprotected_react(path)
 
-        if os.path.exists(absolute_path):
+@jwt_required()
+def serve_protected_react(path: str):
+    current_user = get_jwt_identity()
+    absolute_path = os.path.join(app.static_folder, path)
+    if os.path.exists(absolute_path):
+        if current_user == "admin":
             return send_from_directory(app.static_folder, path)
-
+        if current_user == "data_analyst" and path not in protected_paths_analyst:
+            return send_from_directory(app.static_folder, "index.html")
+        if current_user == "simulation_expert" and path not in protected_paths_simulation:
+            return send_from_directory(app.static_folder, "index.html")
+        return send_from_directory(app.static_folder, "path")
     return send_from_directory(app.static_folder, "index.html")
+
+def serve_unprotected_react(path: str):
+    absolute_path = os.path.join(app.static_folder, path)
+    if os.path.exists(absolute_path):
+        return send_from_directory(app.static_folder, path)
 
 
 if __name__ == "__main__":
