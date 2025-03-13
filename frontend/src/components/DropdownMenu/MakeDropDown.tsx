6f1312a16a6582a64253857da1e18b06d4ec07db
Nils Werner <nils.werner@kabelmail.de>
2025-03-13 12:00:54 +0100

    DropDow for what

---
+++ frontend/src/components/DropdownMenu/MakeDropDown.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Subtract } from 'utility-types';
+import CallBack from './CallBack';
+
+export interface InjectedCounterProps {
+  onChange(): void;
+  open: boolean;
+  text: string[];
+  callBack: CallBack;
+}
+
+interface MakeDropDownProps {
+  text?: string[];
+  callBack: CallBack;
+}
+
+interface MakeCounterState {
+  open: boolean;
+}
+
+const makeDropDown = <P extends InjectedCounterProps>(
+  Component: React.ComponentType<P>
+) => class MakeCounter extends React.Component<
+  Subtract<P, InjectedCounterProps> & MakeDropDownProps,
+  MakeCounterState
+> {
+    state: MakeCounterState = {
+      open: false,
+    };
+
+    change = () => {
+      this.setState(prevState => ({
+        open: !prevState.open
+      }))
+    }
+
+    render() {
+      const { text, callBack, ...props } = this.props;
+      console.log(typeof callBack)
+      console.log(callBack)
+      return (
+        <Component
+          {...props as P}
+          text={text}
+          callBack={callBack}
+          open={this.state.open}
+          onChange={this.change}
+        />
+      );
+    }
+  };
+
+export default makeDropDown;
