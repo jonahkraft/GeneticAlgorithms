4f949ec1db7dc7d3c327688da85059dac1ac05b9
khang <khang.ngy8@gmail.com>
2025-03-14 11:36:28 +0100

    überarbeitung tests

--- frontend/tests/Automatisierte Test für das Endprodukt/AdminEditUser.py
+++ frontend/tests/Automatisierte Test für das Endprodukt/AdminEditUser.py
@@ -6,14 +6,16 @@ import time
 driver = initialize_driver1()
 #driver = initialize_driver2()
 
+oldusername = "khan"
+oldpassword = "nknk"
 
 newusername = "abc"
 newpassword = "123"
 
 
 click_button(driver, "navbar_Login")
-wait_element(driver, By.ID, "username").send_keys(newusername)
-wait_element(driver, By.ID, "password").send_keys(newpassword)
+wait_element(driver, By.ID, "username").send_keys(oldusername)
+wait_element(driver, By.ID, "password").send_keys(oldpassword)
 click_button(driver, "login_Login")
 time.sleep(2)                                           ##dieser Part soll zeigen, dass der User existiert
 click_button(driver, "navbar_Logout")
@@ -27,18 +29,30 @@ click_button(driver, "login_Login")
 time.sleep(2)                                           ##Login als Admin
 click_button(driver, "navbar_Settings")
 click_button(driver, "settings_Management")
-wait_element(driver, By.ID, "settings_OldUser").send_keys(newusername)
-click_button(driver, "settings_DeleteUser")
-driver.execute_script("window.scrollBy(0, -500);")
+wait_element(driver, By.ID, "settings_OldUsername").send_keys(oldusername)
+wait_element(driver, By.ID, "settings_ChangedUsername").send_keys(newusername)
+wait_element(driver, By.ID, "settings_ChangedPassword").send_keys(newpassword)
 time.sleep(3)                                           ##Hier wurde der User vom Admin gelöscht
+click_button(driver, "settings_ChoseRole2")
+click_button(driver, "settings_NewAdmin")
+click_button(driver, "settings_ApplyChanges")
+driver.execute_script("window.scrollBy(0, 500);")
+time.sleep(2)
 click_button(driver, "navbar_Logout")
 alert = Alert(driver)
-time.sleep(2)
 alert.accept()
+time.sleep(2)
 click_button(driver, "navbar_Login")
+wait_element(driver, By.ID, "username").send_keys(oldusername)
+wait_element(driver, By.ID, "password").send_keys(oldpassword)
+click_button(driver, "login_Login")
+time.sleep(2)                                           ##dieser Part soll zeigen, dass der User noch nicht existiert
+wait_element(driver, By.ID, "username").clear()
+wait_element(driver, By.ID, "password").clear()
+time.sleep(2)
 wait_element(driver, By.ID, "username").send_keys(newusername)
+wait_element(driver, By.ID, "password").clear()
 wait_element(driver, By.ID, "password").send_keys(newpassword)
-time.sleep(3)                                           ##Login mit alten Userdaten funktioniert nicht mehr
 click_button(driver, "login_Login")
 time.sleep(3)
 
